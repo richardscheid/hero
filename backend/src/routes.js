0e9b8bd698c1da757f2b4cd6213dc8fedf2babaf
richardscheid <rscheid@live.com>
2020-03-31 11:48:29 -0300

    creating profile route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const ProfileController = require('./controllers/ProfileController');
 const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.list);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.list);
 
 routes.get('/incidents', IncidentController.list);
 routes.post('/incidents', IncidentController.create);
