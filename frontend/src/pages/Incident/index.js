a24b184f053bcb05fd37e2075b21bf8c10bfa0e1
richardscheid <rscheid@live.com>
2020-03-31 15:18:19 -0300

    creating incident page

---
+++ frontend/src/pages/Incident/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+  const history = useHistory();
+
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState('');
+  const [description, setDescription] = useState('');
+  
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Ocorreu um erro ao cadastrar o caso');
+    }
+  }
+
+  return (
+    <div className='new-incident-container'>
+      <div className='content'>
+        <section>
+          <img src={logoImg} alt='Be the Hero' />
+          
+          <h1>Register new case</h1>
+          
+          <p>Describe the case in detail to find a hero to solve this.</p>
+
+          <Link className='back-link' to='/profile'>
+            <FiArrowLeft size={16} color='#E02041' />
+            Back to Home
+          </Link>
+        </section>
+
+        <form>
+          <input
+            placeholder='Title of the case'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder='Description'
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder='Value'
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+
+          <button onClick={handleNewIncident} className='button' type='submit'>
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Incident from './pages/Incident';
 import Register from './pages/Register';
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path='/' exact component={Login} />
         <Route path='/profile' component={Profile} />
         <Route path='/register' component={Register} />
+        <Route path='/incidents/new' component={Incident} />
       </Switch>
     </BrowserRouter>
   );
