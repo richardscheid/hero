5f5b67e04c06dd9c8c15859ccf07a8c7578050a0
richardscheid <rscheid@live.com>
2020-03-31 10:52:52 -0300

    creating route and connection

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+  
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  });
+
+  return res.json({ id });
+});
+
+module.exports = routes;
