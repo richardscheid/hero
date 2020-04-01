c2a362ca2e20c83583db74866eff64b59daf222e
richardscheid <rscheid@live.com>
2020-04-01 14:14:16 -0300

    adding input component to login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -8,6 +8,8 @@ import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
+import Input from '../../components/Input';
+
 export default function Login() {
   const history = useHistory();
   const [name, setName] = useState('');
@@ -16,12 +18,10 @@ export default function Login() {
     e.preventDefault();
 
     try {
-
       const { data } = await api.post('sessions', { name });
       localStorage.setItem('ongId', data.id);
       localStorage.setItem('ongName', name);
       history.push('/profile');
-    
     } catch (err) {
       alert('Something wrong with your Login, try again!');
     }
@@ -34,12 +34,14 @@ export default function Login() {
 
         <form onSubmit={handleLogin}>
           <h1>Sign In</h1>
-          <input
+
+          <Input
+            id='organization'
             placeholder='Your organization'
             value={name}
-            required
             onChange={e => setName(e.target.value)}
           />
+
           <button className='button' type='submit'>
             Login
           </button>
